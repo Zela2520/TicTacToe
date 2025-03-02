6b22b4d6618f8bd66dee35cbf007cf3c55b37a73
Zela2520 <sugarbeatzela@gmail.com>
2025-03-02 22:38:11 +0300

    feat: prettier package

--- tictac/src/app/page.tsx
+++ tictac/src/app/page.tsx
@@ -1,10 +1,22 @@
+import { prisma } from "@/shared/lib/db";
 import { Button } from "@/shared/ui/button";
+import { Card, CardTitle } from "@/shared/ui/card";
 
+export default async function Home() {
+  const games = await prisma.game.findMany();
 
-export default function Home() {
+  console.log({ games });
+
   return (
     <div>
       <Button>hello</Button>
+      {games.map((x) => {
+        return (
+          <Card key={x.id}>
+            <CardTitle>{x.name}</CardTitle>
+          </Card>
+        );
+      })}
     </div>
   );
 }
